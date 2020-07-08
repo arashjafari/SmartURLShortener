be660680c70b926011a5e2782a6dbdec25e61b29
Arash Jafari <arashrj@gmail.com>
2020-07-08 20:20:22 +0430

    show short links in user's dashboard

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Link;
+use Auth;
 
 class UserController extends Controller
 {
     public function showDashboardPage(Request $request)
     {
-        return view('users.dashboard');
+        $user_id = Auth::user()->id;
+        $data = Link::where('user_id', $user_id)->orderBy('id', 'desc')->paginate(25);
+        
+        return view('users.dashboard', ['data' => $data]);
     }
 
 }
--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -9,6 +9,38 @@
 
                 <div class="card-body"> 
                     
+                @if(count($data) == 0)
+                    <div class="alert alert-info" role="alert">
+                    {{ __('There is no information to display!') }}
+                    </div>
+                @else 
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">{{ __('URL') }}</th>
+                            <th scope="col">{{ __('Short') }}</th>
+                            <th scope="col">{{ __('Expire date') }}</th>
+                            <th scope="col">{{ __('Click limitation') }}</th>
+                            <th scope="col">{{ __('Total clicks') }}</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($data as $item)
+                        <tr>
+                            <td>{{ $item->url }}</td> 
+                            <td>{{ config('app.url') }}/{{ $item->custom }}</td> 
+                            <td>{{ $item->expire_at }}</td>  
+                            <td>{{ is_null($item->total_uses) ? '~' : $item->used . '/' . $item->total_uses }}</td>                             
+                            <td>{{ $item->used }}</td> 
+                        </tr>
+                        @endforeach 
+                        </tbody>
+                    </table>
+ 
+                @endif
+
+                {!! $data->links() !!}
+
                 </div> 
             </div> 
         </div>
