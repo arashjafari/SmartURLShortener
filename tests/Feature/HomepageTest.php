9d5bf234d32e77d721072e0e1dfc0974fb81b0eb
Arash Jafari <arashrj@gmail.com>
2020-07-09 17:38:25 +0430

    adds dashboard page tests

--- tests/Feature/HomepageTest.php
+++ tests/Feature/HomepageTest.php
@@ -6,20 +6,28 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseTransactions; 
 use Tests\TestCase;
+use App\User;
 
 class HomepageTest extends TestCase
 {  
-
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
     public function test_homepage_accessible()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200); 
-
+        $response = $this->get('/'); 
+        $response->assertStatus(200);  
     }
+
+    public function test_users_can_access_their_own_dashboard_page()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('user.dashboard')); 
+        $response->assertStatus(200);  
+    }
+
+    public function test_guest_can_not_access_dashboard_page()
+    { 
+        $response = $this->get(route('user.dashboard')); 
+        $response->assertRedirect('/login');
+    }
+
 }
