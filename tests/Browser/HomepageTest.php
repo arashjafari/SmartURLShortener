4cf05275de3df61382aae6499ea788bfeb7c8b8a
Arash Jafari <arashrj@gmail.com>
2020-07-17 02:10:40 +0430

    update home page tests

--- tests/Browser/HomepageTest.php
+++ tests/Browser/HomepageTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Browser;
 
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Str;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use App\Link;
@@ -18,6 +19,11 @@ class HomepageTest extends DuskTestCase
         Artisan::call('migrate:fresh');
     }
  
+    private function getAppURL()
+    {
+        return Str::finish(config('app.url'), '/');
+    }
+
     public function test_home_page_loads_correctly()
     {
         $this->browse(function (Browser $browser) {
@@ -27,26 +33,26 @@ class HomepageTest extends DuskTestCase
     }
 
     public function test_i_can_make_a_short_link()
-    {   
+    {    
         $this->browse(function ($browser) {
             $browser->visit('/')
                     ->type('url', 'https://www.google.com')
                     ->type('customUrl', 'google')
                     ->press('Shorten URL')
                     ->assertPathIs('/')
-                    ->assertSee('http://smarturlshortener/google'); 
+                    ->assertSee($this->getAppURL() . 'google'); 
         
         }); 
     }
 
     public function test_i_can_open_a_short_link_and_redirect_to_main_link()
     {
-
+        
         $link = Link::firstOrCreate(['ip' => '127.0.0.1', 'url' => 'https://www.google.com', 'custom' => 'google']);
  
         $this->browse(function ($browser) use ($link) {
-            $browser->visit( 'http://smarturlshortener/' . $link->custom) 
-                    ->assertHostIs('www.google.com'); 
+            $browser->visit( $this->getAppURL() . $link->custom) 
+                    ->assertHostIs("www.google.com"); 
         
         }); 
     }
